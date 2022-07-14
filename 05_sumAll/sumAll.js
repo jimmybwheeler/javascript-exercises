d76dc3dd78724d3ed3c4f77992bf66c1d2cedc03
jimmy <jimmybwheeler@gmail.com>
2022-07-13 17:45:04 -0700

    completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if(a < 0 || b < 0) return "ERROR";
+    if(a > b){
+        const temp = a;
+        a = b;
+        b = temp;
+    }
+    let total = 0;
+    let high = Math.max(a,b);
+    console.log(high);
+    for(let i = 0; i <= high; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
