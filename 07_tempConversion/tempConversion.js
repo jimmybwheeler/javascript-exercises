c5abe40ba262492a829a0e9006d8860a3770c296
James Wheeler <jimmybwheeler@gmail.com>
2022-07-16 14:57:17 -0700

    completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  if (temperature){
+    let celsius = (temperature - 32) * 5/9;
+    return parseFloat(celsius.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let fahrenheit = (temperature * 9/5) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
