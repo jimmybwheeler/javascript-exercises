1f168e9823509abee210aff29624296bf006ff2c
James Wheeler <jimmybwheeler@gmail.com>
2022-08-26 19:00:32 -0700

    not right

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+    for(let i = 0; i < myString.length; i++){
+        if(myString.charAt(i) == myString.charAt(-i)){
+            return;
+        }
+    } 
 };
 
 // Do not edit below this line
